097f04e6619966b4d357ec294d613bc6306d0197
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:26:43 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/cassandra/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Apache Cassandra.\n" +
+        "Apache Cassandra is an open source NoSQL distributed database trusted by thousands of companies for scalability and high availability without compromising performance.",
+    categories = PluginSubGroup.PluginCategory.DATABASE
+)
+package io.kestra.plugin.cassandra;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
