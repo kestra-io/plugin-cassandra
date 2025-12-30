2de158aa02d320fb1f934e0e9f3affb9f145d8e3
ben8t <pimpaudben@gmail.com>
2025-12-30 15:34:42 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/cassandra/astradb/package-info.java
+++ src/main/java/io/kestra/plugin/cassandra/astradb/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Datastax Astra DB.\n" +
         "Astra DB is an almost zero-friction drop-in replacement for self-managed Cassandra - a very good option for developers looking for a serverless NoSQL database.",
-    categories = PluginSubGroup.PluginCategory.DATABASE
+    categories = PluginSubGroup.PluginCategory.DATABASE,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.cassandra.astradb;
 
