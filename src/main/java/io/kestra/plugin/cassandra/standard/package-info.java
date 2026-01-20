ee215223b9a0ebd3173704aa49658dbd38be28ef
ben8t <pimpaudben@gmail.com>
2026-01-20 09:55:01 +0100

    chore: fix plugin categories

--- src/main/java/io/kestra/plugin/cassandra/standard/package-info.java
+++ src/main/java/io/kestra/plugin/cassandra/standard/package-info.java
@@ -1,7 +1,6 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Apache Cassandra.\n" +
         "Apache Cassandra is an open source NoSQL distributed database trusted by thousands of companies for scalability and high availability without compromising performance.",
-    categories = PluginSubGroup.PluginCategory.DATABASE,
     categories = {
         PluginSubGroup.PluginCategory.DATA
     }
