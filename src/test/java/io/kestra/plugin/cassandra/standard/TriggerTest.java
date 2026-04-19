7eb8069caf8c3ca3f8002472a6c0a4502a1b9885
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-19 15:18:06 +0530

    test: use KestraTest scheduler for trigger tests + evaluate trigger

--- src/test/java/io/kestra/plugin/cassandra/standard/TriggerTest.java
+++ src/test/java/io/kestra/plugin/cassandra/standard/TriggerTest.java
@@ -2,10 +2,9 @@ package io.kestra.plugin.cassandra.standard;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.Optional;
 
+import io.kestra.core.junit.annotations.EvaluateTrigger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -14,15 +13,9 @@ import com.google.common.collect.ImmutableMap;
 
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.executions.Execution;
-import io.kestra.core.queues.QueueFactoryInterface;
-import io.kestra.core.queues.QueueInterface;
-import io.kestra.core.repositories.LocalFlowRepositoryLoader;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
-import io.kestra.core.utils.TestsUtils;
 import jakarta.inject.Inject;
-import jakarta.inject.Named;
-import reactor.core.publisher.Flux;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -43,31 +36,13 @@ public class TriggerTest {
         testHelper.initServer(this.runContext);
     }
 
-    @Inject
-    @Named(QueueFactoryInterface.EXECUTION_NAMED)
-    protected QueueInterface<Execution> executionQueue;
-
-    @Inject
-    protected LocalFlowRepositoryLoader repositoryLoader;
-
-    protected Execution triggerFlow(ClassLoader classLoader, String flowRepository, String flow) throws Exception {
-        CountDownLatch queueCount = new CountDownLatch(1);
-        Flux<Execution> receive = TestsUtils.receive(executionQueue, execution -> {
-            queueCount.countDown();
-            assertThat(execution.getLeft().getFlowId(), is(flow));
-        });
-
-        repositoryLoader.load(Objects.requireNonNull(classLoader.getResource(flowRepository)));
-
-        boolean await = queueCount.await(1, TimeUnit.MINUTES);
-        assertThat(await, is(true));
-
-        return receive.blockLast();
-    }
 
     @Test
-    public void testCassandraTrigger() throws Exception {
-        var execution = triggerFlow(this.getClass().getClassLoader(), "flows", "cassandra-listen");
+    @EvaluateTrigger(flow = "flows/cassandra-listen.yml", triggerId = "watch")
+    public void testCassandraTrigger(Optional<Execution> optionalExecution) {
+        assertThat(optionalExecution.isPresent(), is(true));
+        Execution execution = optionalExecution.get();
+
         var rows = (List<Map<String, Object>>) execution.getTrigger().getVariables().get("rows");
         assertThat(rows.size(), is(1));
     }
