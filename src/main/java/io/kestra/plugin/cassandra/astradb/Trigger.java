9a42c8ff626d5c6b9663167ea24363ffa8d860f4
Malay Dewangan <malaydewangan310@gmail.com>
2026-05-07 17:28:50 +0530

    fix: v2 compatibility

--- src/main/java/io/kestra/plugin/cassandra/astradb/Trigger.java
+++ src/main/java/io/kestra/plugin/cassandra/astradb/Trigger.java
@@ -42,7 +42,7 @@ import lombok.experimental.SuperBuilder;
                     tasks:
                       - id: return
                         type: io.kestra.plugin.core.debug.Return
-                        format: "{{ json(item.value) }}"
+                        format: "{{ fromJson(item.value) }}"
 
                 triggers:
                   - id: watch
